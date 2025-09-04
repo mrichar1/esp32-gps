36102733362d5110b973253c426d2d04458b2a98
Matthew Richardson <M.Richardson@ed.ac.uk>
2025-09-04 23:30:48 +0100

    Fix config options.

--- config.defaults.py
+++ config.defaults.py
@@ -22,16 +22,16 @@ WIFI_PSK = ""                       # PSK for Wifi Access Point
 
 # NTRIP configuration
 # Common config
-NTRIP_MODE = "client"               # To enable NTRIP services, set to one of: client (pull NTRIP data from caster), server (upload NTRIP data to a caster), caster (offer NTRIP data to clients) or caster+server.
-NTRIP_CASTER = "crtk.net"           # NTRIP caster address (for 'server' and 'client' modes).
+NTRIP_MODE = "client"               # To enable NTRIP services, comma-separated list of: client (pull NTRIP data from caster), server (upload NTRIP data to a caster), caster (read/write to servers/clients)
+NTRIP_CASTER = "crtk.net"           # NTRIP caster address (for all modes). For 'caster' this should be the bind address (e.g. 0.0.0.0)
 NTRIP_PORT = 2101                   # NTRIP port (for all modes)
 NTRIP_CREDENTIALS = "c:c"           # NTRIP credentials (in form user:pass). Centipede: "c:c". rtk2go: "your@email.com:none" (for all modes)
-NTRIP_MOUNT = "ESP32"                    # NTRIP mount (for all modes). Note there is no support for NEAR/GGA automatic mountpoints.
+NTRIP_MOUNT = "ESP32"               # NTRIP mount (for all modes). Note there is no support for NEAR/GGA automatic mountpoints.
 
 # Caster config
 # NOTE: The caster only currently supports a single server, mounted at NTRIP_MOUNT, so these values can be fixed in the SOURCETABLE
-NTRIP_CASTER_ADDRESS = "0.0.0.0"    # Address to bind the NTRIP caster
-NTRIP_CASTER_PORT = 2101            # Port to bind the NTRIP caster
+NTRIP_CASTER_BIND_ADDRESS = "0.0.0.0"  # Address to bind the NTRIP caster
+NTRIP_CASTER_BIND_PORT = 2101       # Port to bind the NTRIP caster
 NTRIP_BASE_LATITUDE = "56.62"       # Latitude of NTRIP server (mount)
 NTRIP_BASE_LONGITUDE = "-3.94"      # Longitude of NTRIP server (mount)
 NTRIP_COUNTRY_CODE = "GBR"          # Country code of NTRIP server (mount) e.g. United Kingdom = GBR
