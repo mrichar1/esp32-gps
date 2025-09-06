6ffa4093c0a022f762cf5a17ee7502917230766d
Matthew Richardson <M.Richardson@ed.ac.uk>
2025-09-06 10:34:22 +0100

    Fix config defaults comment.

--- config.defaults.py
+++ config.defaults.py
@@ -23,7 +23,7 @@ WIFI_PSK = ""                       # PSK for Wifi Access Point
 # NTRIP configuration
 # Common config
 NTRIP_MODE = "client"               # To enable NTRIP services, comma-separated list of: client (pull NTRIP data from caster), server (upload NTRIP data to a caster), caster (read/write to servers/clients)
-NTRIP_CASTER = "crtk.net"           # NTRIP caster address (for all modes). For 'caster' this should be the bind address (e.g. 0.0.0.0)
+NTRIP_CASTER = "crtk.net"           # NTRIP caster address (for server and client modes).
 NTRIP_PORT = 2101                   # NTRIP port (for all modes)
 NTRIP_CREDENTIALS = "c:c"           # NTRIP credentials (in form user:pass). Centipede: "c:c". rtk2go: "your@email.com:none" (for all modes)
 NTRIP_MOUNT = "ESP32"               # NTRIP mount (for all modes). Note there is no support for NEAR/GGA automatic mountpoints.
